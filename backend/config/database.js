959216388af57a793b4f86c231623946c1efd1d6
HamzaAhmedSheikh <hamzaahmedsheikh313@gmail.com>
2022-09-08 21:48:45 +0500

    some changes in backend

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,15 +1,19 @@
+const parse = require('pg-connection-string').parse;
+const config = parse(process.env.DATABASE_URL);
+
 module.exports = ({ env }) => ({
-    defaultConnection: "default",
+  connection: {
+    client: 'postgres',
     connection: {
-        client: "postgres",
-        connection: {
-            host: env("DATABASE_HOST", 'ec2-44-210-36-247.compute-1.amazonaws.com'),
-            port: env.int("DATABASE_PORT", 5432),
-            database: env("DATABASE_NAME", "d9bd5hc56lvm5e"),
-            user: env("DATABASE_USERNAME", "ejjscbitnqugmc"),
-            password: env("DATABASE_PASSWORD", "b339bb5e2e77147ef0dfe3365966e7f029d0145d92f13e9df7972e8b68225749"),
-            schema: env("DATABASE_SCHEMA", "public"),
-            ssl: { rejectUnauthorized: false },
-        },
-    }
+      host: config.host,
+      port: config.port,
+      database: config.database,
+      user: config.user,
+      password: config.password,
+      ssl: {
+        rejectUnauthorized: false
+      },
+    },
+    debug: false
+  }  
 });
